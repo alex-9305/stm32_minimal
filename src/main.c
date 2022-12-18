e87538d16643362c035b69d11e7a3a140e22fae5
alex <alexander.letzel@yahoo.de>
2022-12-18 21:23:36 +0100

    reminder: Read current data before writing to cfg

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /* Main program. */
 // TODO: ASSERTS FÜR FUNKTIONEN FÜR EXC HANDLING
-static GPIO_Port_Config gpio_port_a = {0};
 
+// TODO: ZUNÄCHST AKTUELLE WERTE ABFRAGEN IN STRUCTS, BEVOR GESCHRIEBEN WIRD!
+static GPIO_Port_Config gpio_port_a = {0};
 int main(void)
 {
+    gpio_port_a = gpio_read_port_config_to_struct(GPIO_PORT_A);
+    gpio_port_a = gpio_read_port_config_to_struct(GPIO_PORT_B);
+    gpio_port_a = gpio_read_port_config_to_struct(GPIO_PORT_C);
+    gpio_port_a = gpio_read_port_config_to_struct(GPIO_PORT_D);
     int val = 0;
     while (1)
     {
